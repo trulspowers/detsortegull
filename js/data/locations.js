c5c4d9a8385d9a927125244dd0e875a48db27647
Truls Mathiassen <mathiassen.truls@gmail.com>
2026-09-17 18:42:11 +0200

    Add v1 location model

---
+++ js/data/locations.js
@@ -0,0 +1,6 @@
+export const locations = {
+  sandefjord: { name: 'Sandefjord', country: 'NO', currency: 'NOK', stores: ['kiwi','extra','obs','rema1000','meny','spar'] },
+  tromso: { name: 'Tromsø', country: 'NO', currency: 'NOK', stores: ['kiwi','extra','obs','rema1000','eurospar','spar'] },
+  stavanger: { name: 'Stavanger', country: 'NO', currency: 'NOK', stores: ['kiwi','extra','obs','rema1000','meny','spar'] },
+  stromstad: { name: 'Nordby / Strömstad', country: 'SE', currency: 'SEK', stores: ['maximat','nordbymat'] }
+};
