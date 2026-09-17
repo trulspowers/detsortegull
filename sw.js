b77874c58b78962398380ad989ec30cd3236aed4
Truls Mathiassen <mathiassen.truls@gmail.com>
2026-09-17 14:49:02 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='sorte-gull-v2';
+const ASSETS=['/','/manifest.webmanifest','/sorte-gull-bg.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.url.includes('/api/'))return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
